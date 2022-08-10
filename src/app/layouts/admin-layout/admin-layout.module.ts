1735625eda31e0cfc905fc78cb119e011dfbce97
malcolm maima <malcolmmaima@gmail.com>
2022-08-10 14:22:45 +0300

    add calendar component

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -1,20 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { AdminLayoutRoutes } from './admin-layout.routing';
+import { AdminLayoutRoutes } from "./admin-layout.routing";
 
-import { DashboardComponent }       from '../../pages/dashboard/dashboard.component';
-import { UserComponent }            from '../../pages/user/user.component';
-import { TableComponent }           from '../../pages/table/table.component';
-import { TypographyComponent }      from '../../pages/typography/typography.component';
-import { IconsComponent }           from '../../pages/icons/icons.component';
-import { MapsComponent }            from '../../pages/maps/maps.component';
-import { NotificationsComponent }   from '../../pages/notifications/notifications.component';
-import { UpgradeComponent }         from '../../pages/upgrade/upgrade.component';
+import { DashboardComponent } from "../../pages/dashboard/dashboard.component";
+import { UserComponent } from "../../pages/user/user.component";
+import { TableComponent } from "../../pages/table/table.component";
+import { TypographyComponent } from "../../pages/typography/typography.component";
+import { IconsComponent } from "../../pages/icons/icons.component";
+import { MapsComponent } from "../../pages/maps/maps.component";
+import { NotificationsComponent } from "../../pages/notifications/notifications.component";
+import { UpgradeComponent } from "../../pages/upgrade/upgrade.component";
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
+import { CalendarModule, DateAdapter } from "angular-calendar";
+import { adapterFactory } from "angular-calendar/date-adapters/date-fns";
 
 @NgModule({
   imports: [
@@ -22,7 +25,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     RouterModule.forChild(AdminLayoutRoutes),
     FormsModule,
     ReactiveFormsModule,
-    NgbModule
+    NgbModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
   ],
   declarations: [
     DashboardComponent,
@@ -33,7 +40,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     IconsComponent,
     MapsComponent,
     NotificationsComponent,
-  ]
+  ],
 })
-
 export class AdminLayoutModule {}
