b526a1de78d2580b8c8dd5663133f3b4eee2ff2b
malcolm maima <malcolmmaima@gmail.com>
2022-08-10 18:05:25 +0300

    setup calendar UI done, sidebar color change save

--- src/app/shared/fixedplugin/fixedplugin.component.ts
+++ src/app/shared/fixedplugin/fixedplugin.component.ts
@@ -1,32 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { empty } from "rxjs";
 
 @Component({
-    moduleId: module.id,
-    selector: 'fixedplugin-cmp',
-    templateUrl: 'fixedplugin.component.html'
+  moduleId: module.id,
+  selector: "fixedplugin-cmp",
+  templateUrl: "fixedplugin.component.html",
 })
-
-export class FixedPluginComponent implements OnInit{
-
-  public sidebarColor: string = "white";
+export class FixedPluginComponent implements OnInit {
+  public localsetSidebarColor: string;
+  public localsetSidebarActiveColor: string;
+  public sidebarColor: string = "black";
   public sidebarActiveColor: string = "danger";
 
   public state: boolean = true;
 
-  changeSidebarColor(color){
-    var sidebar = <HTMLElement>document.querySelector('.sidebar');
+  changeSidebarColor(color) {
+    //save sidebar color to local storage
+    localStorage.setItem("sidebarColor", color);
+
+    var sidebar = <HTMLElement>document.querySelector(".sidebar");
 
     this.sidebarColor = color;
-    if(sidebar != undefined){
-        sidebar.setAttribute('data-color',color);
+    if (sidebar != undefined) {
+      sidebar.setAttribute("data-color", this.sidebarColor);
     }
   }
-  changeSidebarActiveColor(color){
-    var sidebar = <HTMLElement>document.querySelector('.sidebar');
+  changeSidebarActiveColor(color) {
+    localStorage.setItem("sidebarActiveColor", color);
+    var sidebar = <HTMLElement>document.querySelector(".sidebar");
     this.sidebarActiveColor = color;
-    if(sidebar != undefined){
-        sidebar.setAttribute('data-active-color',color);
+    if (sidebar != undefined) {
+      sidebar.setAttribute("data-active-color", color);
     }
   }
-  ngOnInit(){}
+  ngOnInit() {
+    var sidebar = <HTMLElement>document.querySelector(".sidebar");
+
+    this.localsetSidebarColor = localStorage.getItem("sidebarColor");
+    this.localsetSidebarActiveColor =
+      localStorage.getItem("sidebarActiveColor");
+
+    if (this.localsetSidebarColor != undefined) {
+      this.sidebarColor = this.localsetSidebarColor;
+      sidebar.setAttribute("data-color", this.sidebarColor);
+    }
+
+    if (this.localsetSidebarActiveColor != undefined) {
+      this.sidebarActiveColor = this.localsetSidebarActiveColor;
+      sidebar.setAttribute("data-active-color", this.sidebarActiveColor);
+    }
+
+    console.log("Sidebar active color: " + this.sidebarActiveColor);
+  }
 }
